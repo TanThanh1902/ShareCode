1d6902b9faeb691f6609bbfc177709fddd7a0308
tanhuynh1999 <54873015+tanhuynh1999@users.noreply.github.com>
2020-11-18 14:25:39 +0700

    Giao dien ! Làm tùy bien giao dien

---
+++ ShareCode/Areas/Admin/Controllers/ViewAdminController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ShareCode.Areas.Admin.Controllers
+{
+    public class ViewAdminController : Controller
+    {
+        // GET: Admin/ViewAdmin
+        public ActionResult Index()
+        {
+            return View();
+        }
+        //Menu
+        public PartialViewResult Menu()
+        {
+            return PartialView();
+        }
+    }
+}
